bd22043410771f6e6ff28691e4be9fcd6ff3c87f
Maza-Díaz <jmaza.ivic@gmail.com>
2018-04-06 20:39:49 -0400

    FunciÃn de transformaciÃn proyectiva
    He agregado leedir.m para simplificar varias cosas en el cÃdigo de
    transfomation_b.m(el segundo intento de transformaciÃn) estos cambios
    van a ser implementados en cuadrante.m para poder continuar con el
    analisis.
    
    Deseo ordenar las cosas en cuadrante pero van a ser luego de Ãste
    commit, como: implementar leedir.m, hacer un pre para transformar todo,
    entre otros que no termino de dicidir.

---
+++ leedir.m
@@ -0,0 +1,27 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%       leerdir
+%   Input
+%    d_name = Dirección del directorio de imagenes 'D:\Droplets Video\Cuadricula\cuadricula48G'
+%    filetype = formato de las imagénes 'png'
+%   Output
+%    img_Fnames = el nombre y dirección de los archivos en el directorios
+%
+
+% Esta función busca todos los archivos con la extensión de "filetype" y los coloca en un
+% vector con la dirección completa de cada archivo a conseguido con la
+% extensión
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+function img_FNames = leedir(d_name,filetype)
+
+dir_ = strcat(d_name,'\*.',filetype);
+files_ = dir(dir_);
+
+    for i_=1:length(files_)
+        img_FNames{i_} = strcat(d_name,'\',files_(i_).name);
+    end, clear files_ i_;
+
+end
+
